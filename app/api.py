f2b6ade7310f4970f4d780910294390f294b9825
Richard Weeks <richard.weeks@ons.gov.uk>
2017-03-01 15:04:31 +0000

    Fix api

--- app/api.py
+++ app/api.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from app.resources.message import Message
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,6 +11,7 @@ class HelloWorld(Resource):
         return {'hello': 'world'}
 
 api.add_resource(HelloWorld, '/')
+api.add_resource(Message, '/message/<int:id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
