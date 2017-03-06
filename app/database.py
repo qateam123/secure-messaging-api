608806588116c6769c52cc993815ed2b1da7ad32
tej99 <tejas.patel@qa.com>
2017-03-06 11:48:02 +0000

    creates table if table doesn't exist

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from structlog import get_logger
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData, Table, Column, String, DateTime, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from contextlib import contextmanager
 from sqlalchemy.exc import SQLAlchemyError
@@ -15,6 +15,16 @@ engine = create_engine(settings.SECURE_MESSAGING_DATABASE_URL, convert_unicode=T
 logger.debug("creating database session")
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 base = declarative_base()
+
+if not engine.dialect.has_table(engine, 'secure_message'):  # If table don't exist, Create.
+    metadata = MetaData(engine)
+    # Create a table with the appropriate Columns
+    Table('secure_message', metadata,
+        Column('id', Integer, primary_key=True, nullable=False), Column("msg_to", String),
+        Column("msg_from", String), Column("body", String),
+        Column("submitted_at", DateTime))
+    # Implement the creation
+    metadata.create_all()
 
 def init_db():
     # import all modules here that might define models so that
--- app/settings.py
+++ app/settings.py
@@ -1,2 +1,5 @@
-SECURE_MESSAGING_DATABASE_URL = os.getenv('SECURE_MESSAGING_DATABASE_URL', 'postgres://rasmessage:rasmessage@db:5432/messages')
+import os
+
+SECURE_MESSAGING_DATABASE_URL = os.getenv('SECURE_MESSAGING_DATABASE_URL', 'postgres://rasmessage:rasmessage@localhost:6500/messages')
 #postgres://rasmessage:rasmessage@172.28.93.106:6500/messages
+#postgres://rasmessage:rasmessage@db:5432/messages
