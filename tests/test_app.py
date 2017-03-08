24ac14f819b13f5e3a0e0670ed932dda41e39975
Emilio Ward <warde@C02SQ3S6G8WL.local>
2017-03-08 12:47:27 +0000

    added unit tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,11 @@
-import sys
 import unittest
+from flask import json
+import requests
+import sys
 sys.path.append('../secure-messaging-api')
 from app import api
-
+from app import settings
+from sqlalchemy import create_engine
 
 class FlaskTestCase(unittest.TestCase):
 
@@ -18,7 +21,7 @@ class FlaskTestCase(unittest.TestCase):
         # creates a test client
         self.app = api.app.test_client()
         # propagate the exceptions to the test client
-        self.app.testing = True
+        #self.app.testing = True
 
     def test_home_status_code(self):
         # sends HTTP GET request to the application
@@ -26,6 +29,32 @@ class FlaskTestCase(unittest.TestCase):
         result = self.app.get('/')
         # assert the status code of the response
         self.assertEqual(result.status_code, 200)
+
+    def test_post_request_message_goes_to_database(self):
+        # post json message written up in the ui
+        url = "http://localhost:5050/message"
+        data = {'to': "Emilio", 'from': "Tej", 'body': "Hello World"}
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post(url, data=json.dumps(data), headers=headers)
+        self.assertEqual(response.json(), {'status': "ok"})
+
+
+    def test_that_checks_post_request_is_within_database(self):
+        #check if json message is inside the database
+        engine = create_engine(settings.SECURE_MESSAGING_DATABASE_URL, echo=True)
+        data={}
+        with engine.connect() as con:
+            request = con.execute('SELECT * FROM secure_message WHERE id = (SELECT MAX(id) FROM secure_message)')
+            for row in request:
+                data={"to": row['msg_to'], "from": row['msg_from'], "body": row['body']}
+                # print("to:", row['msg_to'], "from:", row['msg_from'], "body:", row['body'])
+                self.assertEqual({'to': 'Emilio', 'from': 'Tej', 'body': 'Hello World'},data)
+                #con.close()
+
+    # def tearDown(self):
+    #     # Closing down the database
+    #     self.db.session.remove()
+    #     self.db.drop_all()
 
 if __name__ == '__main__':
     unittest.main()
