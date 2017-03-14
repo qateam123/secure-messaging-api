f7d67af3acc086318e44a96276ec2c95b83b3a6f
Richard Weeks <richard.weeks@ons.gov.uk>
2017-03-14 15:55:09 +0000

    Add docstring.

--- app/resources/health.py
+++ app/resources/health.py
@@ -2,5 +2,7 @@ from flask_restful import Resource
 
 class Health(Resource):
 
+    """Rest endpoint to provide health monitoring statistics"""
+
     def get(self):
         return "{status : healthy}"
